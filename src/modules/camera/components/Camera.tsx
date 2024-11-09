8294cf18301ca21f50a4a0befb6985d651cc660a
geekfrontend <geekfrontend16@gmail.com>
2024-11-09 22:03:14 +0800

    feat: add webcam

--- src/modules/camera/components/Camera.tsx
+++ src/modules/camera/components/Camera.tsx
@@ -2,13 +2,21 @@
 
 import PageHeader from "@/components/common/PageHeader";
 import PageWrapper from "@/components/common/PageWrapper";
+import { Button } from "@/components/ui/button";
+import Webcam from 'react-webcam'
 
 export default function Camera() {
   return (
     <>
       <PageHeader isBackButton title="Camera" />
-      <PageWrapper>
-        <div>Camera</div>
+      <PageWrapper className="min-h-screen">
+        <div className="rounded-xl">
+          <Webcam mirrored={true}  className="rounded-xl"/>
+         <div className="mt-4 flex justify-center gap-4 items-center">
+          <Button variant="secondary" className="w-full max-w-32">Cancel</Button>
+          <Button className="w-full max-w-32">Take a Photo</Button>
+         </div>
+        </div>
       </PageWrapper>
     </>
   );
