d6f1d8056702b547b8d7e701cd39789b1f2abb23
geekfrontend <geekfrontend16@gmail.com>
2024-11-12 22:34:00 +0800

    add page 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import NotFound from "@/modules/not-found";
+import Wrapper from "@/components/common/Wrapper";
+
+export default function HomePage() {
+  return (
+    <Wrapper>
+      <NotFound />
+    </Wrapper>
+  );
+}
---
+++ src/modules/not-found/components/NotFound.tsx
@@ -0,0 +1,26 @@
+import PageWrapper from "@/components/common/PageWrapper";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <>
+      <PageWrapper className="bg-background">
+      <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+      <div className="w-full space-y-6 text-center">
+        <div className="space-y-3">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl animate-bounce">404</h1>
+          <p className="text-gray-500">Page not found</p>
+        </div>
+        <Link
+          href="/"
+          className="inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          prefetch={false}
+        >
+          Go back home
+        </Link>
+      </div>
+    </div>
+      </PageWrapper>
+    </>
+  );
+}
